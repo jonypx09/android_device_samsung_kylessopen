641904cb7b745db277a4443ad982e67380700f92
András Oravecz <oraveczandrew@gmail.com>
2015-01-08 13:17:01 +0100

    init

--- recovery/recovery_keys.c
+++ recovery/recovery_keys.c
@@ -5,14 +5,7 @@
 #include "extendedcommands.h"
 
 int device_toggle_display(volatile char* key_pressed, int key_code) {
-    int alt = key_pressed[KEY_LEFTALT] || key_pressed[KEY_RIGHTALT];
-    if (alt && key_code == KEY_L)
-        return 1;
-    // allow toggling of the display if the correct key is pressed, and the display toggle is allowed or the display is currently off
-    if (ui_get_showing_back_button()) {
-        return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_END);
-    }
-    return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
+    return 0;
 }
 
 int device_handle_key(int key_code, int visible) {
@@ -25,11 +18,7 @@ int device_handle_key(int key_code, int visible) {
                 return HIGHLIGHT_DOWN;
 
             case KEY_POWER:
-                if (ui_get_showing_back_button()) {
-                    return SELECT_ITEM;
-                }
-                if (!get_allow_toggle_display() && !ui_root_menu)
-                    return GO_BACK;
+                return SELECT_ITEM;
                 break;
 
 	    case KEY_HOME:
@@ -37,7 +26,6 @@ int device_handle_key(int key_code, int visible) {
 
             case KEY_END:
             case KEY_BACK:
-                if (!get_allow_toggle_display() && !ui_root_menu)
                     return GO_BACK;
         }
     }
